bed3137993738bf666b5b079838f32cc5c8f3214
Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>
2022-08-23 19:53:51 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/projetoabiv/ProjetoabivApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetoabiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoabivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoabivApplication.class, args);
+	}
+
+}

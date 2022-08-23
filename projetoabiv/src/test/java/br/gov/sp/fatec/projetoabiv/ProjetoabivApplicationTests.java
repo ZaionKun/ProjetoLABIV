eec9c46737096be6176b2f61c92c031e3ea49178
Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>
2022-08-23 19:14:06 -0300

    first commit

---
+++ projetoabiv/src/test/java/br/gov/sp/fatec/projetoabiv/ProjetoabivApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetoabiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoabivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
